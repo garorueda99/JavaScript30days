a9f6dd01b9897535318a2dabc0742c19599fadec
Andres Rueda <garo99@gmail.com>
2020-03-05 09:23:42 -0500

    Turn it off

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,6 +1,20 @@
 window.addEventListener('keydown', e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing'); // jquery = key.addClass('playing')
 });
+
+function removeTransition(e){
+    // console.log(e); Many events at the time
+    if(e.propertyName !== 'transform') return; //because transform is the longest
+    this.classList.remove('.playing');
+
+}; 
+
+const keys = document.querySelectorAll(".key");
+// const keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys);
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
