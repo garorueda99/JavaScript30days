1c0d24d1336db9e061a67d683586b6ba86e6be75
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-03-05 22:23:16 -0500

    eventListener

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,11 @@
+var keys
+function removeTransition(e){
+    ; //Many events at the time
+    if(e.propertyName !== 'transform') return; //because transform is the longest
+    this.classList.remove('.playing');
+    console.log('papi papi')
+}; 
+
 window.addEventListener('keydown', e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -7,20 +15,17 @@ window.addEventListener('keydown', e => {
     key.classList.add('playing'); // jquery = key.addClass('playing')
 });
 
-function removeTransition(e){
-    // console.log(e); Many events at the time
-    if(e.propertyName !== 'transform') return; //because transform is the longest
-    this.classList.remove('.playing');
-
-}; 
 
 
 //Because this is placed on the top of HTML the key class are not found 
-function readKeys () {
-    const keys = document.querySelectorAll(".key")
+
+function load(){
+    console.log('hello');
+    const keys = document.querySelectorAll(".key");
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     console.log (keys);
-};
+}
 
+document.addEventListener("DOMContentLoaded", load, false)
 // const keys = Array.from(document.querySelectorAll('.key'));
 
