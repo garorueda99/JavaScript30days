5dbd762601880150b1234004545da0703ea167e2
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-03-09 22:38:39 -0400

    day 2

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,14 +1,16 @@
-
+var totalSeconds = 0;
 function setDate(){
+    
     const secondHand = document.querySelector('.seconds-hand');
     const minsHand = document.querySelector('.min-hand');
     const hourHand = document.querySelector('.hour-hand');
 
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsInDegrees = ((360 / 60 ) * seconds)+ 90;
-    // console.log(`sec ${secondsInDegrees}`);
-        secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
+    totalSeconds === 0 ? totalSeconds = seconds: totalSeconds += 1; 
+    // console.log(`total ${totalSeconds} sec ${seconds}`);
+    const secondsInDegrees = (360 /60) * totalSeconds;
+    secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
 
     const minutes = now.getMinutes();
     const minutesInDegrees = ((360 / 60) * (minutes+(seconds/60)) + 90);
@@ -20,5 +22,7 @@ function setDate(){
     const hoursInDegrees = ((360 / 12) * (hours+(minutes/60)) +  90);
     // console.log(hoursInDegrees);
     hourHand.style.transform = `rotate(${hoursInDegrees}deg)`;
+
+    
 }
 setInterval(setDate, 1000);
