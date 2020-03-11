c916ee0373f7eb291d3501a9c958fc64dcdc300b
Andres Rueda <garo99@gmail.com>
2020-03-11 12:48:05 -0400

    Day 5

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -67,12 +67,18 @@ console.log(alphaByLastName);
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
-const reducerTransport = (accumulator, data) => {
-    if (!accumulator[data]) {
-        accumulator[data]=0;
-    }
-    accumulator[data]++;
-    return accumulator;
-}
+const reducerTransport = (accumulator, data) => !accumulator[data] ? 
+(accumulator[data]=1, accumulator)
+:(accumulator[data]++, accumulator);
+
+// (accumulator, data) => {
+//     if (!accumulator[data]) {
+//         accumulator[data]=1;
+//         return accumulator;
+//     } else {
+//         accumulator[data]++;
+//         return accumulator;
+//     }
+// }    
 const transportation = data.reduce(reducerTransport,{});
 console.log(transportation);
