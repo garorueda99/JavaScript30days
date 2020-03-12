ff77654fce1bcc5b4b3657922b0011f1377aad3c
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-03-11 21:15:06 -0400

    Day 5 final

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,5 +1,14 @@
-const panels = document.querySelector('.panels');
+function load(){
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+}
 
 function toggleOpen(){
-    
+    this.classList.toggle('open');
+}
+
+function toggleActive(e){
+    e.propertyName.includes('flex') ? this.classList.toggle('open-active'):"";
 }
