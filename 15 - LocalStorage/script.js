53abf200081ae2c4ff802f14d5331eac0ed468cd
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-05-12 22:36:23 -0400

    15

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,16 +1,46 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
-function addItem(e){
-    e.preventDefault();
-    const text = (this.querySelector('[name=item]')).value;
-    const item = {
-        text: 'Item Name',
-        done: false
-    }
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text, //short hand property ES6
+    done: false,
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items)); //need to be a string
+  this.reset();
 }
 
-addItems.addEventListener('submit', addItem);
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, index) => {
+      return `
+      <li>
+        <input type="checkbox" data-index=${index} id="item${index}" ${
+        plate.done ? "checked" : ""
+      } />
+        <label for="item${index}">${plate.text}</label>
+      </li>
+      `;
+    })
+    .join(""); //make the array a String
+}
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; //filter other events
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+populateList(items, itemsList);
 
 // console.log(addItems)
